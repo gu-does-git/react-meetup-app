8efdb4d76970c63dae059c5fd16f309aab84da0a
Lokitodev <lokitodev@gmail.com>
2021-12-18 23:43:21 -0300

    Added skeletons, lazyloading and fade animations to the project

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,11 +1,19 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import FavoritesContext from "../../store/favorites-context";
 import Card from "../ui/Card";
 import classes from "./MeetupItem.module.css";
+import LazyLoad from "react-lazyload";
+import ContentLoader from "react-content-loader";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
 function MeetupItem(props) {
   const favoritesCtx = useContext(FavoritesContext);
   const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+  const [imageIsLoaded, setImageIsLoaded] = useState(false);
+
+  function handleImageLoaded() {
+    setImageIsLoaded(true);
+  }
 
   function toggleFavoriteStatusHandler() {
     if (itemIsFavorite) {
@@ -25,7 +33,42 @@ function MeetupItem(props) {
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
+          <LazyLoad height={320} once offset={200}>
+            {!imageIsLoaded && (
+              <motion.div
+                initial={{ opacity: 1 }}
+                variants={{
+                  true: { opacity: 0 },
+                  false: { opacity: 1 },
+                }}
+                animate={!imageIsLoaded.toString()}
+                transition={{ duration: 0.5 }}
+              >
+                <ContentLoader speed={2} height={320} style={{ width: "100%" }}>
+                  <rect
+                    x="0"
+                    y="0"
+                    rx="5"
+                    ry="5"
+                    height="320"
+                    style={{ width: "100%" }}
+                  />
+                </ContentLoader>
+              </motion.div>
+            )}
+            <motion.img
+              initial={{ opacity: 0 }}
+              variants={{
+                true: { opacity: 1 },
+                false: { opacity: 0 },
+              }}
+              animate={imageIsLoaded.toString()}
+              transition={{ duration: 0.5 }}
+              src={props.image}
+              alt={props.title}
+              onLoad={handleImageLoaded}
+            />
+          </LazyLoad>
         </div>
         <div className={classes.content}>
           <h3>{props.description}</h3>
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,7 +1,11 @@
 import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
+import ContentLoader from "react-content-loader";
 
 function AllMeetupsPage() {
+  const runCallback = (cb) => {
+    return cb();
+  };
   const [isLoading, setIsloading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
@@ -29,19 +33,49 @@ function AllMeetupsPage() {
       });
   }, []);
 
+  let content;
   if (isLoading) {
-    return (
+    content = (
       <section>
-        <h1>Loading...</h1>
+        <h1>All Meetups</h1>
+        {runCallback(() => {
+          const row = [];
+          for (var i = 0; i < 10; i++) {
+            row.push(
+              <ContentLoader
+                key={i}
+                speed={2}
+                height={620}
+                style={{ width: "100%" }}
+              >
+                <rect x="0" y="350" rx="4" ry="4" width="200" height="13" />
+                <rect x="0" y="375" rx="4" ry="4" width="120" height="8" />
+                <rect x="0" y="395" rx="4" ry="4" width="100" height="8" />
+                <rect x="0" y="430" rx="4" ry="4" width="150" height="40" />
+                <rect
+                  x="0"
+                  y="0"
+                  rx="5"
+                  ry="5"
+                  height="320"
+                  style={{ width: "100%" }}
+                />
+              </ContentLoader>
+            );
+          }
+          return row;
+        })}
+      </section>
+    );
+  } else {
+    content = (
+      <section>
+        <h1>All Meetups</h1>
+        <MeetupList meetups={loadedMeetups}></MeetupList>
       </section>
     );
   }
-  return (
-    <section>
-      <h1>All Meetups</h1>
-      <MeetupList meetups={loadedMeetups}></MeetupList>
-    </section>
-  );
+  return content;
 }
 
 export default AllMeetupsPage;
